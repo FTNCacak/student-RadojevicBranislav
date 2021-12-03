8bf3bf2bbaa45614f94ac4b35ba3b7ec55c4c679
RadojevicBranislav <72319334+RadojevicBranislav@users.noreply.github.com>
2021-12-03 18:24:27 +0100

    Kreirana metoda za vracanje podataka iz baze

---
+++ DataLayer/StudentRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAllStudents()
+        {
+            List<Student> results = new List<Student>();
+
+            using (SqlConnection sqlconnection = new SqlConnection(Constants.connString)
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+
+                sqlCommand.Connection = sqlconnection;
+            }
+
+
+            return results;
+        }
+    }
+}
