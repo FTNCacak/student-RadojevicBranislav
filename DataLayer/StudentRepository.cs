ac7d7e3ec44c5daed6229573b2ddcb566724eaec
RadojevicBranislav <72319334+RadojevicBranislav@users.noreply.github.com>
2021-12-03 19:21:12 +0100

    Zavrsen BusinessLayer - odradjene fje

--- DataLayer/StudentRepository.cs
+++ DataLayer/StudentRepository.cs
@@ -14,15 +14,49 @@ namespace DataLayer
         {
             List<Student> results = new List<Student>();
 
-            using (SqlConnection sqlconnection = new SqlConnection(Constants.connString)
+            using (SqlConnection sqlconnection = new SqlConnection(Constants.connString))
             {
                 SqlCommand sqlCommand = new SqlCommand();
 
                 sqlCommand.Connection = sqlconnection;
+
+                sqlCommand.CommandText = "SELECT * FROM Students";
+
+                sqlconnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while(sqlDataReader.Read())
+                {
+                    Student student = new Student();
+
+                    student.Id = sqlDataReader.GetInt32(0);
+
+                    student.Name = sqlDataReader.GetString(1);
+
+                    student.IndexNumber = sqlDataReader.GetString(2);
+
+                    student.AverageMark = sqlDataReader.GetDecimal(3);
+
+                    results.Add(student);
+                }
             }
 
 
             return results;
+        }
+
+
+        public int InsertStudent(Student s)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =string.Format( "INSERT INTO Students VALUES('{0}', '{1}', {2})", s.Name, s.IndexNumber, s.AverageMark);
+
+                return sqlCommand.ExecuteNonQuery();
+            }
         }
     }
 }
